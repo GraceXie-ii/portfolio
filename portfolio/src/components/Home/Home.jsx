1533066d0916d2c190e53da247b4afd3190bc9e0
Grace X <gracexielingke@gmail.com>
2025-08-24 14:13:49 -0400

    Trying something else

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -8,20 +8,21 @@ export default function Home() {
     <section style={{ position: "relative", zIndex: 10 }}>
       <Container fluid className="home-section" id="home">
         <Container className="home-content">
-          <div className="text-box">
-            <Row>
-              <Col md={7} className="home-header">
-                <Type />
+          <Row>
+            <Col md={7} className="home-header">
+              <div className="text-box">
+                <div><Type /></div>
+
                 <h1 className="heading-name">
                   I am
                   <strong className="main-name"> Grace Xie</strong>
                 </h1>
-              </Col>
-              <Col md={5} className="headshot">
-                <img src={me} alt="Grace Xie" />
-              </Col>
-            </Row>
-          </div>
+              </div>
+            </Col>
+            <Col md={5} className="headshot">
+              <img src={me} alt="Grace Xie" />
+            </Col>
+          </Row>
         </Container>
       </Container>
       <Content/>
