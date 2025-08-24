d21e1af8b3b1cb040f23d3b3b5b67a9262cbfdfb
Grace X <gracexielingke@gmail.com>
2025-08-24 14:06:56 -0400

    Changing css to fix mobile layout

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -8,21 +8,20 @@ export default function Home() {
     <section style={{ position: "relative", zIndex: 10 }}>
       <Container fluid className="home-section" id="home">
         <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <div className="text-box">
+          <div className="text-box">
+            <Row>
+              <Col md={7} className="home-header">
                 <Type />
-
                 <h1 className="heading-name">
                   I am
                   <strong className="main-name"> Grace Xie</strong>
                 </h1>
-              </div>
-            </Col>
-            <Col md={5} className="headshot">
-              <img src={me} alt="Grace Xie" />
-            </Col>
-          </Row>
+              </Col>
+              <Col md={5} className="headshot">
+                <img src={me} alt="Grace Xie" />
+              </Col>
+            </Row>
+          </div>
         </Container>
       </Container>
       <Content/>
